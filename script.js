c8b4a419791c7f695756756c6e6b808c94c6b108
Jakub Jakubowski <kubajot6@wp.pl>
2022-05-25 17:02:21 +0100

    Create animation, style

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-style.css;
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("mousemove", (e) => moveElements(e));
+
+  const clouds = document.querySelectorAll(".clouds");
+  const person = document.querySelector(".person");
+
+  function moveElements(event) {
+    const { clientY, clientX } = event;
+
+    clouds.forEach((cloud) => {
+      const ratioX = cloud.getAttribute("ratioX");
+      const ratioY = cloud.getAttribute("ratioY");
+
+      cloud.style.transform = `translate(${
+        (clientX - window.innerWidth / 2) * ratioX
+      }%,${(clientY - window.innerHeight / 2) * ratioY}%)`;
+    });
+    const ratioX = person.getAttribute("ratioX");
+    const ratioY = person.getAttribute("ratioY");
+
+    person.style.transform = `translate(${
+      (clientX - window.innerWidth / 2) * ratioX
+    }%,${(clientY - window.innerHeight / 2) * ratioY}%)`;
+  }
+});
